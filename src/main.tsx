87c718d9fb38585ee3d6f95673cca1432dbccb18
Matleox <68650899+Matleox@users.noreply.github.com>
2025-07-26 19:21:51 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx';
+import Home from './Home.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<App />} />
+        <Route path="/panel" element={<App />} />
+        <Route path="/admin" element={<App />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
